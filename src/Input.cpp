254a0fd5f6e99f76c60c83a89ee65fab18d236f9
janumaruku <jean-marc.kottin@ade-labs.com>
2026-02-13 15:25:21 +0100

    chore: setup pin for Input component

--- src/Input.cpp
+++ src/Input.cpp
@@ -12,5 +12,6 @@ Input::Input(std::string name): ASpecialComponent{std::move(name)}
 {
     _type = INPUT_TYPE;
     _value = UNDEFINED;
+    _pins[1] = Pin{PinType::INPUT, nullptr, 0};
 }
 } // nts
