f1548b1459d56d26e9f41d46007305acb268fad5
Liam <liam.harfouche@epitech.eu>
2026-02-20 15:44:44 +0100

    feat: xor gate

---
+++ include/components/elementary/Xor.hpp
@@ -0,0 +1,24 @@
+/*
+** EPITECH PROJECT, 2026
+** OOP
+** File description:
+** 
+*/
+
+#ifndef ANDGATE_HPP
+#define ANDGATE_HPP
+
+#include <memory>
+#include "AElementaryComponent.hpp"
+#include "components/IComponent.hpp"
+
+namespace nts {
+
+class Xor : public AElementaryComponent {
+public:
+    explicit Xor(std::string name);
+    static std::unique_ptr<IComponent> create(std::string name);
+};
+}
+#endif
+
---
+++ src/components/elementary/Xor.cpp
@@ -0,0 +1,28 @@
+/*
+** EPITECH PROJECT, 2026
+** OOP
+** File description:
+** 
+*/
+
+#include "components/elementary/Xor.hpp"
+#include <memory>
+#include <string>
+
+namespace nts {
+
+Xor::Xor(std::string name) : AElementaryComponent(std::move(name)) {
+        this->_type = AND_TYPE;
+        this->_truthTable = {
+            {Tristate::FALSE, Tristate::TRUE, Tristate::UNDEFINED},
+            {Tristate::TRUE, Tristate::FALSE, Tristate::UNDEFINED},
+            {Tristate::UNDEFINED, Tristate::UNDEFINED, Tristate::UNDEFINED},
+        };
+    }
+
+std::unique_ptr<IComponent> Xor::create(std::string name)
+{
+    return std::make_unique<Xor>(std::move(name));    
+}
+
+}
