e5457a9499ade109d722d7a54f95a86176303b84
Liam <liam.harfouche@epitech.eu>
2026-02-20 17:27:09 +0100

    fix: needed to reset pins for not

--- src/components/elementary/Not.cpp
+++ src/components/elementary/Not.cpp
@@ -13,9 +13,11 @@
 namespace nts {
 
 Not::Not(std::string name) : AElementaryComponent(std::move(name)) {
-        this->_type = NOR_TYPE;
-        this->_truthTable = {Tristate::TRUE, Tristate::FALSE, Tristate::UNDEFINED};
-    }
+    this->_type = NOT_TYPE;
+    this->_truthTable = {Tristate::TRUE, Tristate::FALSE, Tristate::UNDEFINED};
+    this->_pins[1] = Pin(PinType::INPUT, nullptr, 0);
+    this->_pins[2] = Pin(PinType::OUTPUT, nullptr, 0);
+}
 
 std::unique_ptr<IComponent> Not::create(std::string name)
 {
@@ -27,9 +29,8 @@ Tristate Not::compute(const std::size_t &pin)
     if (pin != 2) 
         throw std::logic_error("Invalid pin for Not gate");
     Tristate input = Tristate::UNDEFINED;
-    if (this->_pins[1].linkedComponent == nullptr)
-        throw std::logic_error("Not gate not linked");
-    input = this->_pins[1].linkedComponent->compute(this->_pins[1].pin);
+    if (this->_pins[1].linkedComponent != nullptr)
+        input = this->_pins[1].linkedComponent->compute(this->_pins[1].pin);
     return this->_truthTable[input];
 }
 
