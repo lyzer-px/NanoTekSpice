d02231f39145cae59ae57a23ca84e5eda84bb792
Liam <liam.harfouche@epitech.eu>
2026-02-23 15:09:31 +0100

    feat: six inverter

---
+++ src/components/composite/SixInverter.cpp
@@ -0,0 +1,29 @@
+/*
+** EPITECH PROJECT, 2026
+** NanoTekSpice
+** File description:
+** SixInverter
+*/
+
+#include "components/composite/SixInverter.hpp"
+
+#include "components/elementary/Nand.hpp"
+
+namespace nts {
+SixInverter::SixInverter(std::string name): ACompositeComponent{std::move(name)}
+{
+    _type = FOUR_NAND_TYPE;
+
+    for (std::size_t i = 0; i < 6; ++i)
+        _components.push_back(
+            std::make_unique<Nand>("nand_" + std::to_string(i + 1)));
+
+    ACompositeComponent::registerInternComponentsPins();
+
+    ACompositeComponent::setPinsType();
+}
+std::unique_ptr<IComponent> SixInverter::create(std::string name)
+{
+    return std::make_unique<SixInverter>(std::move(name));
+}
+} // namespace nts
