d89f3bdcd68d4785fba9c73e065f3c99a41c639f
Liam <liam.harfouche@epitech.eu>
2026-02-23 11:18:39 +0100

    fix: nor gate truth table

--- src/components/elementary/Nor.cpp
+++ src/components/elementary/Nor.cpp
@@ -14,7 +14,7 @@ namespace nts {
 Nor::Nor(std::string name) : AElementaryComponent(std::move(name)) {
         this->_type = NOR_TYPE;
         this->_truthTable = {
-            {Tristate::FALSE, Tristate::TRUE, Tristate::UNDEFINED},
+            {Tristate::FALSE, Tristate::FALSE, Tristate::UNDEFINED},
             {Tristate::TRUE, Tristate::TRUE, Tristate::UNDEFINED},
             {Tristate::UNDEFINED, Tristate::UNDEFINED, Tristate::UNDEFINED},
         };
