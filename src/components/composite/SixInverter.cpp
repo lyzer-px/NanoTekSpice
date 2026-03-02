d2026de1c863a38cc033c142ca2e78cd1d45fecc
Liam <liam.harfouche@epitech.eu>
2026-03-02 15:02:49 +0100

    feat: six inverter

--- src/components/composite/SixInverter.cpp
+++ src/components/composite/SixInverter.cpp
@@ -7,45 +7,52 @@
 
 #include "components/composite/SixInverter.hpp"
 
+#include "components/IComponent.hpp"
 #include "components/elementary/Not.hpp"
 
 namespace nts {
 
 void SixInverter::setPinsType()
 {
-    for (const auto &pin: {1, 3, 5, 7, 9, 11}) {
+    for (const auto &pin: {1, 3, 5, 9, 11, 13}) {
         _pins[pin] = Pin(PinType::INPUT); 
     }
     for (const auto &pin: {2, 4, 6, 8, 10, 12}) {
         _pins[pin] = Pin(PinType::OUTPUT);
     }
-    _pins[13] = Pin(PinType::POWER);
+    _pins[7] = Pin(PinType::POWER);
+    _pins[14] = Pin(PinType::POWER);
 }
 
 void SixInverter::registerInternComponentsPins()
 {
    _inputPins[1] = std::pair{_components[0].get(), 1}; 
-   _inputPins[2] = std::pair{_components[1].get(), 2}; 
-   _inputPins[3] = std::pair{_components[2].get(), 1}; 
-   _inputPins[4] = std::pair{_components[3].get(), 2}; 
-   _inputPins[5] = std::pair{_components[4].get(), 1}; 
-   _inputPins[6] = std::pair{_components[5].get(), 2}; 
-   _inputPins[7] = std::pair{_components[6].get(), 1}; 
-   _inputPins[13]  = std::pair{nullptr, 0};
+   _inputPins[2] = std::pair{_components[0].get(), 2}; 
+   _inputPins[3] = std::pair{_components[1].get(), 1}; 
+   _inputPins[4] = std::pair{_components[1].get(), 2}; 
+   _inputPins[5] = std::pair{_components[2].get(), 1}; 
+   _inputPins[6] = std::pair{_components[2].get(), 2}; 
+   _inputPins[7] = std::pair{nullptr, 0}; 
+   _inputPins[8] = std::pair{_components[3].get(), 2}; 
+   _inputPins[9] = std::pair{_components[3].get(), 1}; 
+   _inputPins[10] = std::pair{_components[4].get(), 2}; 
+   _inputPins[11] = std::pair{_components[4].get(), 1}; 
+   _inputPins[12] = std::pair{_components[5].get(), 2}; 
+   _inputPins[13] = std::pair{_components[5].get(), 1}; 
+   _inputPins[14] = std::pair{nullptr, 0}; 
 }
 
 SixInverter::SixInverter(std::string name): ACompositeComponent{std::move(name)}
 {
-    _type = FOUR_NAND_TYPE;
+    _type = SIX_INVERTER_TYPE;
 
     for (std::size_t i = 0; i < 6; ++i)
         _components.push_back(
             std::make_unique<Not>("not_" + std::to_string(i + 1)));
-
-    ACompositeComponent::registerInternComponentsPins();
-
-    ACompositeComponent::setPinsType();
+    this->registerInternComponentsPins();
+    this->setPinsType();
 }
+
 std::unique_ptr<IComponent> SixInverter::create(std::string name)
 {
     return std::make_unique<SixInverter>(std::move(name));
