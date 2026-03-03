34fd8de07f18fd0d021af28c943513b855626087
Liam <liam.harfouche@epitech.eu>
2026-03-03 13:08:56 +0100

    feat: unit_tests

---
+++ include/Exception/Exceptions.hpp
@@ -0,0 +1,27 @@
+/*
+** EPITECH PROJECT, 2026
+** OOP
+** File description:
+** 
+*/
+
+#ifndef EXCEPTIONS_HPP
+#define EXCEPTIONS_HPP
+
+#include "BadFileExtensionException.hpp"
+#include "ChipsetAlreadyDefinedError.hpp"
+#include "ChipsetArgumentError.hpp"
+#include "ChipsetCommandNotFound.hpp"
+#include "ChipsetSyntaxError.hpp"
+#include "FileNotFoundException.hpp"
+#include "LinkDefinitionSyntaxError.hpp"
+#include "LinkNoPinNumber.hpp"
+#include "LinkSyntaxError.hpp"
+#include "LinkUnknownChipset.hpp"
+#include "NoChipsetError.hpp"
+#include "ParsingException.hpp"
+#include "PinNumberIsInteger.hpp"
+#include "UnknowChipsetType.hpp"
+#include "UnknownChipsetType.hpp"
+
+#endif // !EXCEPTIONS_HPP
