097f42ce8fa3133c8f68f82ddfbd285c699e7046
janumaruku <jean-marc.kottin@ade-labs.com>
2026-03-03 11:23:50 +0100

    chore: use the half adder to create the full bit adder

--- src/components/composite/FullBitAdder.cpp
+++ src/components/composite/FullBitAdder.cpp
@@ -13,6 +13,7 @@
 #include <string>
 
 #include "../../../include/components/IComponent.hpp"
+#include "../../../include/components/composite/HalfAdder.hpp"
 #include "../../../include/components/elementary/And.hpp"
 #include "../../../include/components/elementary/Or.hpp"
 #include "../../../include/components/elementary/Xor.hpp"
@@ -24,10 +25,8 @@ FullBitAdder::FullBitAdder(std::string name): ACompositeComponent{
     _type        = FULL_ADDER_TYPE;
     _numberOfPin = 5;
 
-    _components.push_back(std::make_unique<Xor>("xor_1"));
-    _components.push_back(std::make_unique<Xor>("xor_2"));
-    _components.push_back(std::make_unique<And>("and_1"));
-    _components.push_back(std::make_unique<And>("and_2"));
+    _components.push_back(std::make_unique<HalfAdder>("half_adder_1"));
+    _components.push_back(std::make_unique<HalfAdder>("half_adder_2"));
     _components.push_back(std::make_unique<Or>("or"));
 
     FullBitAdder::registerInternComponentsPins();
@@ -42,18 +41,15 @@ std::unique_ptr<IComponent> FullBitAdder::create(std::string name)
 
 void FullBitAdder::registerInternComponentsPins()
 {
+    _components[0]->setLink(4, *(_components[2].get()), 1);
     _components[0]->setLink(3, *(_components[1].get()), 1);
-    _components[0]->setLink(3, *(_components[3].get()), 1);
-    _components[2]->setLink(3, *(_components[4].get()), 2);
-    _components[3]->setLink(3, *(_components[4].get()), 1);
+    _components[1]->setLink(4, *(_components[2].get()), 2);
 
-    _internalPins[1] = {std::pair{_components[0].get(), 1},
-                        std::pair{_components[2].get(), 1}};
-    _internalPins[2] = {std::pair{_components[0].get(), 2},
-                        std::pair{_components[2].get(), 2}};
+    _internalPins[1] = {std::pair{_components[0].get(), 1}};
+    _internalPins[2] = {std::pair{_components[0].get(), 2}};
     _internalPins[3] = {std::pair{_components[1].get(), 2}};
     _internalPins[4] = {std::pair{_components[1].get(), 3}};
-    _internalPins[5] = {std::pair{_components[4].get(), 3}};
+    _internalPins[5] = {std::pair{_components[2].get(), 3}};
 }
 
 void FullBitAdder::setPinsType()
