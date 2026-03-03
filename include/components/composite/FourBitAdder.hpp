c5cee7db37d54abf774593bacb188fdd780d25cf
janumaruku <jean-marc.kottin@ade-labs.com>
2026-03-03 12:29:31 +0100

    feat: create the four bit adder

---
+++ include/components/composite/FourBitAdder.hpp
@@ -0,0 +1,26 @@
+/*
+** EPITECH PROJECT, 2026
+** NanoTekSpice
+** File description:
+** FourBitAdder
+*/
+
+#ifndef NANOTEKSPICE_FOURBITADDER_HPP
+#define NANOTEKSPICE_FOURBITADDER_HPP
+#include "ACompositeComponent.hpp"
+
+namespace nts {
+class FourBitAdder: public ACompositeComponent {
+public:
+    explicit FourBitAdder(std::string name);
+
+    static std::unique_ptr<IComponent> create(std::string name);
+
+private:
+    void registerInternComponentsPins() override;
+
+    void setPinsType() override;
+};
+} // nts
+
+#endif //NANOTEKSPICE_FOURBITADDER_HPP
